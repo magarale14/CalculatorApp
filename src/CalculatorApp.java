1260d5c421563041844b8b0d46ea6f98ee24eb64
Reeta Ale Magar <41391452+magarale14@users.noreply.github.com>
2018-08-14 16:17:28 -0400

    Initial commit

---
+++ src/CalculatorApp.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class CalculatorApp {
+	public static void main(String args[]){
+		Scanner reeta = new Scanner(System.in);
+		double fnum,snum,answer;
+		System.out.println("Enter first number: ");
+		fnum = reeta.nextDouble();
+		System.out.println("Enter second number: ");
+		snum = reeta.nextDouble();
+		answer = fnum + snum;
+		System.out.println(answer);
+	
+		}
+		
+	}
+
